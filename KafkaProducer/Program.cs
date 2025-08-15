ae14f4c7f4a7e8d778e630e3bf162e1e2b78c2c6
Anastasija Mosina <anastasija.mosina@sainsburys.co..uk>
2025-08-15 13:19:49 +0100

    add producer

---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,48 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaProducer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092"
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                Console.WriteLine("Kafka Producer started.");
+                Console.WriteLine("Type a message and press Enter to send to 'quickstart-events' topic.");
+                Console.WriteLine("Type 'exit' to quit.");
+
+                string input;
+                while ((input = Console.ReadLine()) != null)
+                {
+                    if (input.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                        break;
+
+                    try
+                    {
+                        var result = await producer.ProduceAsync(
+                            "quickstart-events",
+                            new Message<Null, string> { Value = input });
+
+                        Console.WriteLine(
+                            $"Delivered '{input}' to: {result.TopicPartitionOffset}");
+                    }
+                    catch (ProduceException<Null, string> e)
+                    {
+                        Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ KafkaProducer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KafkaProducer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sainsbury's Supermarkets Limited")]
+[assembly: AssemblyProduct("KafkaProducer")]
+[assembly: AssemblyCopyright("Copyright © Sainsbury's Supermarkets Limited 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("082f670a-20d2-4383-a0fc-a43e4c4b91cd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
