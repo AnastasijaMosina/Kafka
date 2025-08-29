5689cef0b1a366d6657f72d7baa0e9977ff3587c
Anastasija Mosina <anastasija.mosina@sainsburys.co..uk>
2025-08-29 11:09:18 +0100

    refactor - switch to Sdk + remove redundant code,

--- KafkaProducer/Interfaces/IRandomGenerator.cs
+++ KafkaProducer/Interfaces/IRandomGenerator.cs
@@ -1,9 +1,4 @@
 ﻿using KafkaProducer.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KafkaProducer.Interfaces
 {
