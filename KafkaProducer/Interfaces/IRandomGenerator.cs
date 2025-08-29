283cb6f7333f675f407103c3b178170d79348805
Anastasija Mosina <anastasija.mosina@sainsburys.co..uk>
2025-08-29 10:23:05 +0100

    create order random generator

---
+++ KafkaProducer/Interfaces/IRandomGenerator.cs
@@ -0,0 +1,14 @@
+﻿using KafkaProducer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaProducer.Interfaces
+{
+    public interface IRandomGenerator
+    {
+        OrderEvent GenerateRandomOrder();
+    }
+}
---
+++ KafkaProducer/Utils/RandomGenerator.cs
@@ -0,0 +1,28 @@
+﻿using KafkaProducer.Interfaces;
+using KafkaProducer.Models;
+using System;
+
+namespace KafkaProducer.Utils
+{
+    internal class RandomGenerator : IRandomGenerator
+    {
+        private readonly Random _random = new Random();
+
+        public OrderEvent GenerateRandomOrder()
+        {
+            var products = new[] { "Laptop", "Mouse", "Keyboard", "Monitor", "Headphones" };
+            var statuses = new[] { "PENDING", "PROCESSING", "SHIPPED", "DELIVERED" };
+
+            return new OrderEvent
+            {
+                OrderId = _random.Next(1000, 9999),
+                CustomerId = $"CUST-{_random.Next(100, 999)}",
+                Amount = Math.Round((decimal)(_random.NextDouble() * 1000 + 50), 2),
+                Status = statuses[_random.Next(statuses.Length)],
+                Timestamp = DateTime.UtcNow,
+                ProductName = products[_random.Next(products.Length)],
+                Quantity = _random.Next(1, 5)
+            };
+        }
+    }
+}
