6ae323ea2d453dab74d7db8f4be89633e44f7a89
Anastasija Mosina <anastasija.mosina@sainsburys.co..uk>
2025-08-29 10:22:35 +0100

    create events data model

---
+++ KafkaProducer/Models/OrderEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaProducer.Models
+{
+    public class OrderEvent
+    {
+        public int OrderId { get; set; }
+        public string CustomerId { get; set; }
+        public decimal Amount { get; set; }
+        public string Status { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
